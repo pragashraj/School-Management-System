de7e1188fa2364e8c160d8b2b725d0e291bac91e
S.Pragashraj <rajpragashraj30@gmail.com>
2020-04-18 07:38:42 +0530

    user reducer, user action

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import {BrowserRouter,Switch,Route} from 'react-router-dom'
 import Rootpage from './Pages/RootPage'
 import Home from './Pages/Home'
+import NavBar from './Components/NavBar'
 
 const App=()=> {
   return (
     <BrowserRouter>
         <div className="App">
+            <NavBar/>
             <Switch>
-                <Route exact path="/" component={Rootpage}/>
-                <Route  path="/home" component={Home}/>
+                <Route exact path="/signin" component={Rootpage}/>
+                <Route  path="/" component={Home}/>
             </Switch>
         </div>
     </BrowserRouter>
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import '../CSS/Nav/navBar.css'
+import user from '../assets/users.png'
+import logo from '../assets/logo.png'
+import {Link} from 'react-router-dom'
 
 const NavBar=()=> {
     return (
         <div className="nav-bar">
+           <img src={logo} alt="logo" className="logo-image"/>
+           <div className="nav-options">
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="#"  className="nav-link">SAU</Link>
+                <Link to="#"  className="nav-link">TAU</Link>
+                <Link to="#"  className="nav-link">About Us</Link>
+                <Link to="#"  className="nav-link">Contact Us</Link>
+                <Link to="/signin"  className="nav-link">SignIn</Link>
+                <img src={user} alt="log-out" className="logout-image"/>
+           </div>
            
         </div>
     )
